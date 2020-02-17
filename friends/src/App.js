55f094e49362c6895c1a10751cbbe750b88707ce
Kristi Gribble <kgribble4083@gmail.com>
2020-02-16 20:16:12 -0600

    Initial Commit and basic files created

---
+++ friends/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./components/Login";
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
